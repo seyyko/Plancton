9499c3c1566f40f97014aa7192cf79b29d78a691
unknown <hchamrouk.pro@gmail.com>
2024-12-09 22:51:57 +0100

    add a secret key

--- src/back_end/__init__.py
+++ src/back_end/__init__.py
@@ -7,7 +7,11 @@ def create_app():
     app = Flask(__name__, template_folder='../front_end/templates', static_folder='../front_end/static')
 
     # Configure the application (you can add your configuration here)
-    app.config['SECRET_KEY'] = 'secret_key'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_key')
+    # you can set you own secret key (e.g. in powershell)
+    # $env:SECRET_KEY = "my_secret_key"
+    # after the manipulation, uncomment below to see if it was succeessful!
+    # print("⚠️  IMPORTANT :", os.getenv('SECRET_KEY', 'default_key'))
     
     # Import and register the application's routes
     import routes
